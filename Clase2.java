3f34ac9acc778fc1d326610333f524f173734585
oscar_c <oscar_carrillo@cun.edu.co>
2026-04-08 14:56:33 -0500

    clase2

--- Clase2.java
+++ Clase2.java
@@ -2,40 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Clase1 {
+public class Clase2 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        PuedeVotar();
 
-        String nombre = "DAVID";
-        double dinero = 1.000;
-        System.out.print(nombre + " tienes este dinero:" + "$ " + dinero);
-
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(i);
-        }
-
-        PuedeVotar(20);
-        PuedeVotar(16);
-        PuedeVotar(18);
-        PuedeVotar(17);
-        
-        String nombreSolicitado = solicitarNombre();
-        System.out.println("Su nombre es: " + nombreSolicitado);
-        
-        SolicitarEdad();
     }
-    // validar si la persona puede votar
 
-    public static void PuedeVotar(int edad) {
-
-        System.out.print("Su Edad es:" + edad + " ");
+    public static void PuedeVotar() {
+        String nombre = solicitarNombre();
+        int edad = SolicitarEdad();
 
         if (edad >= 18) {
-            System.out.println("Puede Votar");
+            System.out.println(nombre + " puedes votar");
         } else {
-            System.out.println("lo sentimos no puede Votar");
+            System.out.println(nombre + " no puedes votar");
         }
     }
 
@@ -51,16 +33,17 @@ public class Clase1 {
         }
     }
 
-    public static void SolicitarEdad() {
+    public static int SolicitarEdad() {
         System.out.println("Ingrese su edad");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try {
             int edad = Integer.parseInt(reader.readLine());
+            return edad;
         } catch (NumberFormatException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return 0;
     }
-
 }
